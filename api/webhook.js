7956a19ab4237ed9262311bf1159f8a0071da11e
Panot Wongkhot <panotza_ja@hotmail.com>
2019-09-16 01:13:04 +0700

    add log

--- api/webhook.js
+++ api/webhook.js
@@ -1,11 +1,13 @@
 const line = require('../service/line')
+const insertLog = require('../service/db')
+const client = line.init(process.env.CHANNEL_ACCESS_TOKEN, process.env.CHANNEL_SECRET)
 
-function handleEvent(event) {
+async function handleEvent(event) {
+	await insertLog(event)
+
 	if (event.type !== 'message' || event.message.type !== 'text') {
 		return Promise.resolve(null)
 	}
-
-	const client = line.init(process.env.CHANNEL_ACCESS_TOKEN, process.env.CHANNEL_SECRET)
 
 	return client.replyMessage(event.replyToken, {
 		type: 'text',
---
+++ service/db.js
@@ -0,0 +1,8 @@
+const MongoClient = require('mongodb').MongoClient
+
+module.exports = async function insertLog (item) {
+	const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true })
+	await client.connect()
+	await client.db('ava').collection('logs').insertOne(item)
+	client.close()
+}
